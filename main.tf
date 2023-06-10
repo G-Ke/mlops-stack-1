aa47ed07d27a9bb2f68b9fbdbfe61bb68409e52b
G-Ke <george.kenneally@outlook.com>
2023-06-10 04:50:13 -0400

    Changes to main.tf image names

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ terraform {
 provider "docker" {
 }
 
-resource "docker_image" "mlo2-image" {
-  name = "mlo2:latest"
+resource "docker_image" "mlops-image-1" {
+  name = "mlops-1:latest"
 }
 
-resource "docker_container" "mlo2-container" {
-  image = docker_image.mlo2-image.image_id
-  name  = "MLOps-2"
+resource "docker_container" "mlops-container-1" {
+  image = docker_image.mlops-image-1.image_id
+  name  = "MLOps-1"
   ports {
     internal = 80
     external = 8000
