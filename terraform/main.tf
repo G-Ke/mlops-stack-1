2b7cf5cba36d6b35ef0547a53b0b1a77dbca11da
G-Ke <george.kenneally@outlook.com>
2023-06-16 09:35:25 -0400

    Making changes to Terraform config

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+    cloud {
+        organization = "g-ke"
+        workspaces {
+            name = "mlops-stack-1"
+        }
+    }
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = ">=5.4.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+}
+
+module "ec2_instance" {
+    source  = "terraform-aws-modules/ec2-instance/aws"
+
+    name = "single-instance"
+
+    instance_type          = "t2.micro"
+    key_name               = "MyEC2KP"
+    monitoring             = false
+    vpc_security_group_ids = ["sg-0aae88811984b6ba2"]
+
+    tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    }
+}
