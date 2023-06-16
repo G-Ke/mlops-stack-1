d6805d14697802a0609198c38e989682a1f3aa9c
G-Ke <george.kenneally@outlook.com>
2023-06-16 11:12:39 -0400

    Making changes to Terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,18 +17,6 @@ provider "aws" {
     region = "us-east-1"
 }
 
-module "ec2_instance" {
-    source  = "terraform-aws-modules/ec2-instance/aws"
-
-    name = "single-instance"
-
-    instance_type          = "t2.micro"
-    key_name               = "MyEC2KP"
-    monitoring             = false
-    vpc_security_group_ids = ["sg-0aae88811984b6ba2"]
-
-    tags = {
-    Terraform   = "true"
-    Environment = "dev"
-    }
+resource "aws_ecr_repository" "mlops-1-ecr" {
+    name = "mlops-stack-1"
 }
