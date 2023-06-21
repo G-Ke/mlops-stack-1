a0e92401a26d92f4b16e75725574ed19e64d97f5
G-Ke <george.kenneally@outlook.com>
2023-06-21 16:13:21 -0400

    Testing Terraform configurations.

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,8 +103,8 @@ resource "aws_network_acl" "mlops-stack-VPC-nacl" {
         rule_no    = 100
         action     = "allow"
         cidr_block = "0.0.0.0/0"
-        from_port  = "all"
-        to_port    = "all"
+        from_port  = "0"
+        to_port    = "0"
     }
 
     tags = {
