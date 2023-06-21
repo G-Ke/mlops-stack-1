a3490e524f218e52d4c90f9a07cc12f308f09b14
G-Ke <george.kenneally@outlook.com>
2023-06-21 10:38:42 -0400

    Testing Terraform configs for ECS via TF Cloud.

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,19 +18,19 @@ provider "aws" {
 }
 
 resource "aws_ecr_repository" "mlops-ecr" {
-    name = "mlops-stack-1"
+    name = "MLOps-Stack-2"
 }
 
-resource "aws_ecs_cluster" "mlops-stack-1-ecs" {
-    name = "mlops-stack-1"
+resource "aws_ecs_cluster" "mlops-stack-2-ecs" {
+    name = "MLOps-Stack-2"
 }
 
-resource "aws_ecs_task_definition" "mlops-1-task" {
-    family = "mlops-1-task"
-    container_definitions = <<DEFINITION
+resource "aws_ecs_task_definition" "mlops-stack-2-taskdef" {
+    family                   = "MLOps-Stack-2"
+    container_definitions    = <<DEFINITION
     [
         {
-            "name": "mlops-1-task",
+            "name": "MLOps-Stack-2-TaskDef",
             "image": "${aws_ecr_repository.mlops-ecr.repository_url}",
             "essential": true,
             "portMappings": [
@@ -51,41 +51,42 @@ resource "aws_ecs_task_definition" "mlops-1-task" {
     ]
     DEFINITION
     requires_compatibilities = ["FARGATE"]
-    network_mode = "awsvpc"
-    memory = 512
-    cpu = 256
-    execution_role_arn = "arn:aws:iam::343725977869:role/ecsTaskExecutionRole"
+    network_mode             = "awsvpc"
+    cpu                      = "256"
+    memory                   = "512"
+    execution_role_arn       = "arn:aws:iam::343725977869:role/ecsTaskExecutionRole"
+    task_role_arn            = "arn:aws:iam::343725977869:role/ecsTaskExecutionRole"
 }
 
-resource "aws_default_vpc" "default_vpc" {
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    name               = "MLOps-Stack-2-vpc"
+    cidr               = "10.0.0.0/22"
+    azs                = ["us-east-1a", "us-east-1d"]
+    private_subnets    = ["10.0.8.0/22", "10.0.10.0/22"]
+    public_subnets     = ["10.0.0.0/22", "10.0.1.0/22"]
+    enable_nat_gateway = true
+    enable_vpn_gateway = false
+    enable_ipv6        = false
+    tags               = {Project = "MLOps-Stack-2"}
 }
 
-resource "aws_default_subnet" "default_subnet_a" {
-    availability_zone = "us-east-1a"
-}
-
-resource "aws_default_subnet" "default_subnet_b" {
-    availability_zone = "us-east-1b"
-}
-
-resource "aws_alb" "application_load_balancer" {
-    name = "mlops-load-balancer"
+resource "aws_alb" "mlops-alb" {
+    name = "MLOps-Stack-2-alb"
     load_balancer_type = "application"
-    subnets = [
-        "${aws_default_subnet.default_subnet_a.id}",
-        "${aws_default_subnet.default_subnet_b.id}"
-    ]
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+    subnets = module.vpc.public_subnets
+    security_groups = [aws_security_group.mlops-stack-2-alb-sg.id]
 }
 
-resource "aws_security_group" "load_balancer_security_group" {
+resource "aws_security_group" "mlops-stack-2-alb-sg" {
+    name = "MLOps-Stack-2-alb-sg"
+    vpc_id = module.vpc.vpc_id
     ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
     egress {
         from_port = 0
         to_port = 0
@@ -94,50 +95,64 @@ resource "aws_security_group" "load_balancer_security_group" {
     }
 }
 
-resource "aws_lb_target_group" "target_group" {
-    name = "mlops-target-group"
+resource "aws_lb_target_group" "mlops-stack-2-tg" {
+    name = "MLOps-Stack-2-tg"
     port = 80
     protocol = "HTTP"
+    vpc_id = module.vpc.vpc_id
     target_type = "ip"
-    vpc_id = "${aws_default_vpc.default_vpc.id}"
-}
-
-resource "aws_lb_listener" "listener" {
-    load_balancer_arn = "${aws_alb.application_load_balancer.arn}"
-    port = "80"
-    protocol = "HTTP"
-    default_action {
-        type = "forward"
-        target_group_arn = "${aws_lb_target_group.target_group.arn}"
+    health_check {
+        path = "/"
+        port = "traffic-port"
+        protocol = "HTTP"
+        matcher = "200-399"
+        interval = 30
+        timeout = 5
     }
 }
 
-resource "aws_ecs_service" "mlops-1-service" {
-    name = "mlops-1-service"
-    cluster = "${aws_ecs_cluster.mlops-stack-1-ecs.id}"
-    task_definition = "${aws_ecs_task_definition.mlops-1-task.family}"
+resource "aws_lb_target_group_attachment" "mlops-stack-2-tg-attachment" {
+    target_group_arn = aws_lb_target_group.mlops-stack-2-tg.arn
+    target_id = aws_alb.mlops-alb.arn
+    port = 80
+}
+
+resource "aws_lb_listener" "mlops-stack-2-listener" {
+    load_balancer_arn = aws_alb.mlops-alb.arn
+    port = 80
+    protocol = "HTTP"
+    default_action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.mlops-stack-2-tg.arn
+    }
+}
+
+resource "aws_ecs_service" "mlops-stack-2-service" {
+    name = "MLOps-Stack-2-service"
+    cluster = aws_ecs_cluster.mlops-stack-2-ecs.id
+    task_definition = aws_ecs_task_definition.mlops-stack-2-taskdef.family
     launch_type = "FARGATE"
     desired_count = 1
 
     load_balancer {
-        target_group_arn = "${aws_lb_target_group.target_group.arn}"
-        container_name = "${aws_ecs_task_definition.mlops-1-task.family}"
+        target_group_arn = aws_lb_target_group.mlops-stack-2-tg.arn
+        container_name = aws_ecs_task_definition.mlops-stack-2-taskdef.family
         container_port = 80
     }
 
     network_configuration {
-        subnets = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+        subnets = module.vpc.public_subnets
+        security_groups = [aws_security_group.mlops-stack-2-sg.id]
         assign_public_ip = true
-        security_groups = ["${aws_security_group.ecs_service_security_group.id}"]
     }
 }
 
-resource "aws_security_group" "ecs_service_security_group" {
+resource "aws_security_group" "mlops-stack-2-sg" {
     ingress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+        security_groups = [aws_security_group.mlops-stack-2-alb-sg.id]
     }
 
     egress {
@@ -148,6 +163,6 @@ resource "aws_security_group" "ecs_service_security_group" {
     }
 }
 
-output "mlops-stack-url" {
-    value = aws_alb.application_load_balancer.dns_name
+output "mlops-stack-2-url" {
+    value = aws_alb.mlops-alb.dns_name
 }
