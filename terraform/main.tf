ab16503be53bd5fc5203630c13b662bf4f02f0e0
G-Ke <george.kenneally@outlook.com>
2023-06-21 17:43:00 -0400

    Testing Terraform configurations.

--- terraform/main.tf
+++ terraform/main.tf
@@ -208,8 +208,9 @@ resource "aws_lb_listener" "mlops-stack-alb-listener" {
 
 resource "aws_lb_target_group" "mlops-stack-alb-tg" {
     name = "MLOps-Stack-ALB-TG"
-    port = 80
     target_type = "alb"
+    port = 80
+    protocol = "HTTP"
     vpc_id = module.vpc.vpc_id
 }
 
