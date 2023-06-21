d21bfeda706ba192cc0e3d01514a706cda03ef38
G-Ke <george.kenneally@outlook.com>
2023-06-21 15:46:24 -0400

    Testing Terraform configurations.

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,3 +32,20 @@ module "vpc" {
         Project = "MLOps-Stack"
     }
 }
+
+resource "aws_security_group" "mlops-stack-VPC-sg" {
+    name = "MLOps-Stack-VPC-sg"
+    vpc_id = module.vpc.vpc_id
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
