f30082705932eb08cf96962ca668596f5fb630bf
G-Ke <george.kenneally@outlook.com>
2023-06-23 19:02:20 -0400

    Testing Terraform configs for ECS via TF Cloud.

--- terraform/main.tf
+++ terraform/main.tf
@@ -192,10 +192,8 @@ resource "aws_lb_listener" "mlops-stack-alb-listener" {
     protocol = "TCP"
     default_action {
         type = "forward"
-        forward {
-            target_group {
-                arn = aws_lb_target_group.mlops-stack-alb-tg.arn
-            }
+        target_group {
+            arn = aws_lb_target_group.mlops-stack-alb-tg.arn
         }
     }
 }
